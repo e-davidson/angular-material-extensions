14ca9c148f7a21700bd8271cd1cba93f861198de
Reuvain Levin <reuvainlevin@gmail.com>
2019-08-20 21:58:59 -0400

    bug fix to the num between function

---
+++ projects/table-builder/src/lib/components/number-filter/number-filter.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FilterInfo } from '../../classes/filter-info';
+import { debounceTime } from 'rxjs/operators';
+import { FilterType } from '../../enums/filterTypes';
+
+
+@Component({
+  selector: 'tb-number-filter',
+  templateUrl: './number-filter.component.html',
+  styleUrls: ['./number-filter.component.css']
+})
+export class NumberFilterComponent implements OnInit {
+  @Input() info: FilterInfo;
+  change$ = new EventEmitter();
+  @Output() filter$ = this.change$.pipe(debounceTime(250));
+  FilterType = FilterType;
+
+  @Input() set CurrentFilterType(ft: FilterType) {
+    if (ft === FilterType.NumberBetween) {
+      this.info.filterValue = {};
+    }
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
