6504d43b2c22b83f3cfacf4e44de61c28f0b206a
Nachman Berkowitz <nberkowitz@broadfinancial.com>
2021-01-04 16:52:22 -0500

    done

--- projects/table-builder/src/lib/directives/resize-column.directive.ts
+++ projects/table-builder/src/lib/directives/resize-column.directive.ts
@@ -1,32 +1,26 @@
 import { Directive, OnInit, Renderer2, Input, ElementRef } from "@angular/core";
 import { ComponentStore } from "@ngrx/component-store";
-import { fromEvent, Observable } from "rxjs";
-import { map, switchMap, tap } from "rxjs/operators";
+import { fromEvent, Observable, of, pipe } from "rxjs";
+import { first, map, mergeMap, switchMap, tap } from "rxjs/operators";
 import { TableStore } from "../classes/table-store";
 
 @Directive({
-  selector: "[resizeColumn]"
+  selector: "[resizeColumn]",
 })
 export class ResizeColumnDirective extends ComponentStore<ResizeColumnDirectiveState>  implements OnInit{
   @Input("resizeColumn") resizable: boolean;
 
   @Input() key: string;
-  // private startX: number;
-
-  // private startWidth: number;
-  // private startTableWidth:number;
-
-  // private columnHead: HTMLElement;
-  // private table: HTMLElement;
-
-  // private pressed: boolean;
 
   constructor(private renderer: Renderer2, private el: ElementRef, public state: TableStore,) {
     super({...new ResizeColumnDirectiveState(), columnHead: el.nativeElement})
   }
- 
-  ngOnInit = this.effect((nothing: Observable<never>) => nothing.pipe(
-    switchMap(() => this.selectColumnHead.pipe(
+  ngOnInit(){
+    this.setup();
+  }
+  setup = this.effect((nothing: Observable<never>) => {
+    return nothing.pipe(
+    switchMap(() => this.selectColumnHeadOnce.pipe(
       tap((ch) => {
         if (this.resizable) {
           this.setState(state => {
@@ -35,46 +29,53 @@ export class ResizeColumnDirective extends ComponentStore<ResizeColumnDirectiveS
             this.state.on(fromEvent(table, "mousemove"), this.onMouseMove)
             return ({ ...state, table });
           })
-          // const row = this.renderer.parentNode(this.columnHead);
-          // this.table = this.renderer.parentNode(row);
           const resizer = this.renderer.createElement("span");
           this.renderer.addClass(resizer, "resize-holder");
           this.renderer.appendChild(ch, resizer);
           this.state.on(fromEvent<MouseEvent>(resizer, "mousedown").pipe(
-            switchMap((event)=> this.selectColumnHead.pipe(map(((columnHead)=> ({columnHead,event})))))), this.onMouseDown);
+            switchMap((event)=> this.selectColumnHeadOnce.pipe(map(((columnHead)=> ({columnHead,event})))))), this.onMouseDown);
           this.state.on(fromEvent(document, "mouseup"), this.onMouseUp)
         }
 
-      })))))
+      }))))}
+      )
 
   onMouseDown = (ev:{event: MouseEvent,columnHead:HTMLElement}) => {
-    this.pressed = true;
-    this.startX = ev.event.pageX;
-    this.startWidth = ev.columnHead.offsetWidth;
-    this.startTableWidth = this.table.offsetWidth;
+    this.setState(state => ({...state,
+      pressed:true,
+      startX : ev.event.pageX,
+      startWidth : ev.columnHead.offsetWidth,
+      startTableWidth : state.table.offsetWidth,
+    }));
   };
 
-  onMouseMove = (event: MouseEvent) => {
-    const offset = 13;
-    if (this.pressed && event.buttons) {
-
-      // Calculate width of column
-      const change = (event.pageX - this.startX)
-      let width = (this.startWidth + change) - offset;
-      let tableSize = (this.startTableWidth + change) - offset;
-
-      this.state.setUserDefinedWidth([{key:this.key,widthInPixel:width}]);
-      this.state.setTableWidth(tableSize);
-    }
-  };
+  onMouseMove = this.effect((mouseMove$: Observable<MouseEvent>) => 
+    mouseMove$.pipe(mergeMap(event => this.selectStateOnce.pipe(tap(state => {
+      {
+        const offset = 13;
+        if (state.pressed && event.buttons) {
+          // Calculate width of column
+          const change = (event.pageX - state.startX)
+          let width = (state.startWidth + change) - offset;
+          if(width < 1){
+            width= 1;
+          }
+          const columnChange = width - state.startWidth;
+          let tableSize = (state.startTableWidth + columnChange);
+    
+          this.state.setUserDefinedWidth([{key:this.key, widthInPixel:width}]);
+          this.state.setTableWidth(tableSize);
+        }
+      }
+    }))))
+  );
 
   onMouseUp = (event: MouseEvent) => {
-    if (this.pressed) {
-      this.pressed = false;
-    }
+    
+    this.setState(state => ({...state,pressed : false}));
   };
-
-  selectColumnHead = this.select(state => state.columnHead);
+  selectStateOnce = this.select(state => state).pipe(first());
+  selectColumnHeadOnce = this.selectStateOnce.pipe(map(state => state.columnHead));
 }
 
 class ResizeColumnDirectiveState {
