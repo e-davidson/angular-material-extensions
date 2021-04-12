6da385ee21c0d4b4bbc686e6665f1f6ea1c86a55
Nachman Berkowitz <nberkowitz@broadfinancial.com>
2021-04-12 16:25:39 -0400

    yona

--- projects/table-builder/src/lib/pipes/space-case.pipes.ts
+++ projects/table-builder/src/lib/pipes/space-case.pipes.ts
@@ -3,7 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name: 'spaceCase'})
 export class SpaceCasePipe implements PipeTransform {
   transform(value: string): string {
-    return  value && value.replace(/([A-Z])/g, ' $1')
+    return  value?.replace(/([A-Z])/g, ' $1')
     // uppercase the first character
     .replace(/^./,  str => str.toUpperCase() );
   }
