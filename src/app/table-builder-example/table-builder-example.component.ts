4f61cd28ada1957cc5dc842bebf3df0c163192c2
Nachman Berkowitz <38742354+NachmanBerkowitz@users.noreply.github.com>
2021-06-17 10:07:49 -0400

    clear_filters (#109)
    * updated angular
    
    * updated angular material
    
    Co-authored-by: Nachman Berkowitz <nberkowitz@madisontrust.com>

--- src/app/table-builder-example/table-builder-example.component.ts
+++ src/app/table-builder-example/table-builder-example.component.ts
@@ -25,7 +25,7 @@ export interface PeriodicElement {
   date: Date;
   moreInfo?: string []
   phone: string;
-  W : Weight;
+  W? : Weight;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
@@ -38,7 +38,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
   {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N', gas: true, date: new Date(2020, 9, 7), phone: '23909085656' , W: Weight.hi},
   {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O', gas: false, date: new Date(2020, 9, 1), phone: '8456782345', W: Weight.hi },
   {position: 9, name: 'Neon', weight: 20.1797, symbol: 'Ne', gas: true, date: undefined, phone: '2341234456' , W: Weight.med},
-  {position: null, name: null, weight: 18.9984, symbol: 'F', gas: false, date: new Date(2020, 9, 9), phone: '123456789012' , W: Weight.hi},
+  {position: null, name: null, weight: 18.9984, symbol: 'F', gas: false, date: new Date(2020, 9, 9), phone: '123456789012' },
 ];
 
 const additional: ArrayAdditional = {
@@ -186,5 +186,9 @@ export class TableBuilderExampleComponent {
 
   selectionEvent(a){
     console.log(a)
+  }
+
+  clearAllFilters(){
+    this.tableContainer.state.clearFilters()
   }
 }
