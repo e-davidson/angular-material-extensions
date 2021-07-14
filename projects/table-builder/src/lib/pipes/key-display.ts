e6b7d76b6ce817a7bb1b44a9d74c3b49f0c14301
Nachman Berkowitz <38742354+NachmanBerkowitz@users.noreply.github.com>
2021-07-14 09:44:46 -0400

    tweaked (#123)
    * updated space case pipe
    
    Co-authored-by: Nachman Berkowitz <nberkowitz@madisontrust.com>

--- projects/table-builder/src/lib/pipes/key-display.ts
+++ projects/table-builder/src/lib/pipes/key-display.ts
@@ -1,16 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { TableStore } from '../classes/table-store';
-import { SpaceCasePipe } from './space-case.pipes';
+import { spaceCase } from './space-case.pipes';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Pipe({name: 'keyDisplay'})
 export class KeyDisplayPipe implements PipeTransform {
-  constructor( public tableState: TableStore, private spaceCase: SpaceCasePipe) {
+  constructor( public tableState: TableStore) {
   }
   transform(key: string): Observable<string> {
     return this.tableState.getMetaData$(key).pipe(
-      map( metaData => metaData.displayName || this.spaceCase.transform(key))
+      map( metaData => metaData.displayName || spaceCase(key))
     );
   }
 }
